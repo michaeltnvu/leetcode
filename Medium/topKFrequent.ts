383fcaab99e4067755c75150ab010b2ffbe9d9e4
Michael Vu <vu.michaeltien@gmail.com>
2024-05-20 13:55:15 -0400

    added topKFrequent

---
+++ Medium/topKFrequent.ts
@@ -0,0 +1,37 @@
+// 347. Top K Frequent Elements
+
+// Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,1,1,2,2,3], k = 2
+// Output: [1,2]
+// Example 2:
+
+// Input: nums = [1], k = 1
+// Output: [1]
+ 
+
+// Constraints:
+
+// 1 <= nums.length <= 105
+// -104 <= nums[i] <= 104
+// k is in the range [1, the number of unique elements in the array].
+// It is guaranteed that the answer is unique.
+ 
+
+// Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+
+function topKFrequent(nums: number[], k:number) {
+  const frequencyMap = new Map<number, number>();
+
+  for (const num of nums) {
+    frequencyMap.set(num, ((frequencyMap.get(num) || 0) + 1));
+  }
+
+  const sortedElements = Array.from(frequencyMap.entries()).sort((a, b) => b[1] - a[1]);
+
+  return sortedElements.slice(0, k).map(entry => entry[0]);
+}
