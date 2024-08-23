984a16b464811bd95fb56e8cc1c27a135671f773
Michael Vu <vu.michaeltien@gmail.com>
2024-08-23 14:58:03 -0700

    solution to 0020 accepted

--- 0020-valid-parentheses.js
+++ 0020-valid-parentheses.js
@@ -39,3 +39,31 @@ Constraints:
 1 <= s.length <= 104
 s consists of parentheses only '()[]{}'.
 */
+
+var isValid = function(s) {
+  const stack = [];
+  const map = {
+    ")": "(",
+    "}": "{",
+    "]": "["
+  };
+
+  for (const char of s) {
+    if (char in map) {
+      if (stack.length !== 0 && stack[stack.length - 1] === map[char]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    } else {
+      stack.push(char);
+    }
+  }
+
+  return stack.length === 0;
+}
+
+/* 
+  time complexity: O(n)
+  space complexity: O(n)
+*/
