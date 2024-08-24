51779c1e67a274c5ac8078f69f3204a727a0ef9e
Michael Vu <vu.michaeltien@gmail.com>
2024-08-23 19:21:51 -0700

    solution to 0021 accepted

--- 0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists.js
@@ -28,3 +28,44 @@ The number of nodes in both lists is in the range [0, 50].
 -100 <= Node.val <= 100
 Both list1 and list2 are sorted in non-decreasing order.
 */
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function(list1, list2) {
+  const dummy = new ListNode();
+  let node = dummy;
+
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      node.next = list1;
+      list1 = list1.next;
+    } else {
+      node.next = list2;
+      list2 = list2.next;
+    }
+    node = node.next;
+  }
+
+  if (list1) {
+    node.next = list1;
+  } else {
+    node.next = list2;
+  }
+
+  return dummy.next;
+}
+
+/* 
+  time complexity: O(n + m)
+  space complexity: O(1)
+*/
